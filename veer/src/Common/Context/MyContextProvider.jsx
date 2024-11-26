fc838e4ac0502f79bdcced9a467432636277c682
nanu2705 <nandanipatel057@gmail.com>
2024-11-26 14:44:19 +0530

    front

--- veer/src/Common/Context/MyContextProvider.jsx
+++ veer/src/Common/Context/MyContextProvider.jsx
@@ -9,7 +9,7 @@ const MyContextProvider = ({ children }) => {
   const location = useLocation()
 
   //for backend calling api start
-const url = 'http://localhost:3035'
+const url = 'https://back-zhzt.vercel.app'
 
   // for login drawer open start
   const [isOpen, setIsOpen] = useState(false)
